c49a8ccd7059aab0e319508b7a6dcb65be063ff8
Mlcohol <1977621337@qq.com>
2021-04-08 10:15:37 +0800

    用户注册表单

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $table = 'users';
     protected $fillable = [
         'name',
         'email',
@@ -40,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function gravatar ($size = '100'){
+        $hash = md5(strtolower(trim($this -> attributes['email'])));
+        return "https://www.gravatar.com/avatar/$hash?=$size";
+    }
 }
